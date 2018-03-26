2763acad00b9fadd6ea49b93f957a69aabb25797
Khoi Le <minarek@outlook.com>
2018-03-26 21:36:04 +0200

    Created object method for testing

---
+++ scripts/app.js
@@ -0,0 +1,49 @@
+//App.js
+$(document).ready(function(){
+    //CREATE VALIDATORS into an object - validators:{} and predefine the functions for these validators. We have 2 arguments for every validator input)
+    FormValidator.addValidator('isNumberBiggerThan0',(validatorCurrentValue) => {
+        return (parseInt(validatorCurrentValue) > 0);
+    });
+    FormValidator.addValidator('isNumber0OrBiggerThan0',(validatorCurrentValue) => {
+        return (parseInt(validatorCurrentValue) >= 0);
+    });
+    FormValidator.addValidator('isStringNotNull',(validatorCurrentValue) => {
+        return (validatorCurrentValue.trim() !== "")
+    });
+
+    //MAIN
+    $("#btnAddProduct").click(function () {
+        //Clear all data before validation
+        $("#dspMessage").html("");
+
+        //Check status - Testing purpose
+        const currentValidators = FormValidator.getValidators() || [];
+        console.log("Current validators",currentValidators); //get all current validator names
+
+        //Create validator mapping
+        //(HTML element - VALIDATOR)
+        const mapping = new Map();
+        mapping.set('productId','isNumber0OrBiggerThan0');
+        mapping.set('productName','isStringNotNull');
+        mapping.set('productPrice','isNumberBiggerThan0');
+        mapping.set('stockStatus','isStringNotNull');
+        FormValidator.prepare(mapping);
+        //console.log(mapping);
+
+        //Create data mapping
+        //(HTML element Id - Element name)
+        const data = new Map();
+        data.set('productId','Product Id');
+        data.set('productName','Product name');
+        data.set('productPrice','Product price');
+        data.set('stockStatus','Stock status');
+
+        let result = FormValidator.validate(data);
+        //console.log(data);
+
+        if (result)
+            $("#dspMessage").html("Everything is ok now!").attr("style","color:green");
+        else
+            $("#dspMessage").attr("style","color:red");
+    });
+});
---
+++ scripts/validator.js
@@ -0,0 +1,67 @@
+const FormValidator = {
+    //This is an object and its items are arrays
+    //validators: {[array1],[array2],[array3],...}
+    validators: {},
+
+    addValidator(validatorName, validatorFunction) {
+        // spara undan en validator funktion med med ett givet namn.
+        if (!this.validators[validatorName])
+            this.validators[validatorName] = [];
+
+        this.validators[validatorName].push(validatorFunction);
+    },
+
+    prepare(mapping) {
+        // spara undan ett mappningsobjekt mellan formulärfält och deras validators.
+        this.validatorsMapping = mapping;
+    },
+
+    validate(data) {
+        // för varje property (= formulärfält) i dataobjektet 
+        //  hämta ut motsvarande validator för property:n m.h.a mappningsobjektet och applicera den på property:ns 
+        //   värde (= formulärdata)
+        //  om validatorn returnerar false, skapa och lägg till ett felmeddelande i en lista. Felmeddelandet ska 
+        //   innehålla namnet på property:n.
+        //
+        // returnera listan av felmeddelanden.
+
+        let validationResult = true;
+
+        //Get VALIDATORS mapping
+        let validatorsMapping = this.validatorsMapping;
+
+        //Do not use data mapping
+        console.log("Begin process in validate() function...");
+        validatorsMapping.forEach((item,key)=>{
+            if (this.assignToValidator(item,$("#"+key).val()))
+                console.log(true,key,$("#"+key).val(),item); //Testing purpose
+            else{
+                console.log(false,key,$("#"+key).val(),item); //Testing purpose
+                validationResult = false;
+                //Display error messages
+                $("#dspMessage").append($("<p>").text(data.get(key) + " is not valid"));
+            }
+        });
+
+        return validationResult
+    },
+
+    getValidators(){
+        //Get all current validator's names which added by method addValidator()
+        const validatorNames = [];
+        for (let validatorName in this.validators)
+            validatorNames.push(validatorName);
+        return validatorNames;
+    },
+
+    assignToValidator(validatorName, validatorData){
+        const currentData = this.validators[validatorName];
+        let result = false;
+        currentData.forEach(assignData => {
+            result = assignData(validatorData);
+        });
+        return result
+    }
+};
+
+
